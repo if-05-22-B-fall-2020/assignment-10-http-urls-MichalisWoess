7e8f4e54a4b09611660c14e19239494860935b2d
Michalis Wöss <michaliswoess@gmail.com>
2021-06-15 22:37:08 +0200

    Unit Test 2 + Unit Test 1 Fix
    . wasn't escaped in U1, still worked. No further changes, U2 worked with U1 regex.

--- src/main/java/HttpUrlChecker.java
+++ src/main/java/HttpUrlChecker.java
@@ -34,7 +34,7 @@ public class HttpUrlChecker {
         //https?:\/\/(www\.)?([a-zA-Z0-9]+(\.|\-))+[a-zA-Z]+
         //^ Full Pattern
 
-        Pattern pattern = Pattern.compile("http:\\/\\/[a-z].com");
+        Pattern pattern = Pattern.compile("http:\\/\\/[a-z]\\.com");
         Matcher matcher = pattern.matcher(s);
 
         return matcher;
